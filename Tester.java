b94cf45ae28111ec08bccdb7cc1704a5ff493c13
Hannah-Scheuer <hscheuer20@gmail.com>
2020-10-25 20:40:09 -0400

    I tested sumRows, this time using Arrays.toString and it works

--- Tester.java
+++ Tester.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
 public class Tester{
   public static void main(String arg[]){
     int[] intArray = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
+    int[][] intMatrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
     System.out.println(ArrayOps.sum(intArray));
     System.out.println(ArrayOps.largest(intArray));
+    System.out.println(Arrays.toString(ArrayOps.sumRows(intMatrix)));
   }
 }
